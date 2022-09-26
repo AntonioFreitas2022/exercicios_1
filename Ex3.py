ac99270a7726388ca6159982131e6abd26ad129d
ciberseguranca <antoniolopesfreitas@enta.pt>
2022-09-26 12:12:44 +0000

    Fazer resto exercicios tambem

--- Ex3.py
+++ Ex3.py
@@ -9,6 +9,15 @@ Mostre todos os números que satisfazem o pedido do utilizador
 import random
 
 
+# def divisores(numero):
+#    zeros = 0
+#
+#    for n in range(1, numero + 1):
+#        if numero % n == 0:
+#            zeros += 1  # Isto incrementa zeros por um | zeros + 1
+#    return zeros
+
+
 def get_random(ini, fim):
     """
     Esta função devolve um número aleatório entre ini e fim inclusive
@@ -17,3 +26,40 @@ def get_random(ini, fim):
     :return: número aleatório
     """
     return random.randrange(ini, fim + 1)
+
+
+if __name__ == '__main__':
+    ini = int(input('n1'))
+    fim = int(input('n2'))
+    print(ini, fim)
+    pares = 0
+    impares = 0
+    primos = 0
+    tipo = str(input('pares impares ou primos'))
+    if tipo == 'pares':
+        pares = 1
+    elif tipo == 'impares':
+        impares = 1
+
+    else:
+        primos = 1
+
+    for x in (ini, fim + 1):
+        n_aleatorio = random.randrange(ini, fim + 1)
+        print(n_aleatorio)
+        print(x)
+
+        if pares == 1:
+            if n_aleatorio % 2 == 0:
+                print(n_aleatorio)
+
+
+
+
+        # elif impares == 1:
+        #    if n_aleatorio % 2 != 0:
+        #        print('n_aleatorio')
+        #    else:
+        #        for n in range(ini, fim + 1):
+        #            if divisores(n) == 2:
+        #                print(n)
---
+++ test.py
@@ -0,0 +1,7 @@
+import random
+ini = int(input('n1'))
+fim = int(input('n2'))
+
+n_aleatorio = int(random.randrange(ini, fim + 1))
+
+print(n_aleatorio)
