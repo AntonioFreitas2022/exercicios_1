508309d2d0a723e66c3237080416e341da3f00f9
ciberseguranca <antoniolopesfreitas@enta.pt>
2022-09-27 12:14:11 +0000

    ex 4 resolvido, 5 em processo

--- Ex5.py
+++ Ex5.py
@@ -38,7 +38,11 @@ palavra_invert = ''
 for y in range(0 ,palavras_frase):
     list_palavras.append(palavras_separadas[y])
     for y1 in range(0, len(list_palavras[y])):
-        palavra_invert = list_palavras[y1] + palavra_invert    # Aqui eu quero inverter o cada uma das palavras mas em ordem. COnsigo trocar a ordem das palavras, mas não estou a conseguir inverter a palavra em si
+        print(len(list_palavras[y]))
+        palavra_actual = list_palavras[y1]
+        print(palavra_actual)
+        print(palavra_actual[1])
+        palavra_invert = palavra_actual[y1] + palavra_invert    # Aqui eu quero inverter o cada uma das palavras mas em ordem. COnsigo trocar a ordem das palavras, mas não estou a conseguir inverter a palavra em si
 
     print(palavra_invert)
 
